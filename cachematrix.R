8d1eb882addb7bffe7769ed0f40851bb91f725b5
Ravi Mysore <ravipattabhi@gmail.com>
2020-08-07 23:02:21 +0530

    Changed code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
+## The functions use the cache mechansism to better the perfromance
 ## functions do
 
-## Write a short comment describing this function
+## This funtion is used to cache the matrix information
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
 }
 
 
-## Write a short comment describing this function
+## This is used to resolve the cache functionality
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+}
+##on the  lines of the illustrations provided
+##The functions use the cache mechansism to better the perfromance
+##This function is used to cache the matrix information
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+## This is used to resolve the cache functionality
+
+
+cacheSolve <- function(x, ...) {
+  
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
